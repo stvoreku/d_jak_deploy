c3f73246968aaef32d61499767afda3555b5e3e4
Jacek <stvoreku@gmail.com>
2020-04-05 17:08:51 +0200

    first commit

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@app.get('/method')
+def method_get()
+    return {"method":"GET"}
+
+@app.post('/method')
+def method_post()
+    return {"method":"POST"}
+
+@app.put('/method')
+def method_put()
+    return {"method":"PUT"}
+
+@app.delete('/method')
+def method_delete()
+    return {"method":"DELETE"}
