44d6fd7b8eef890f7776fc333bf062c69e4e94e0
Jacek <stvoreku@gmail.com>
2020-04-05 19:09:45 +0200

    first commit

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from fastapi import FastAPI
 import json
 app = FastAPI()
+from pydantic import BaseModel
+
+class Patient(BaseModel):
+    name: str
+    surename: str
+
+
 with open('json_data', 'w') as file:
     try:
         data = json.load(file.read)
     except:
         data = []
+USER_NUM = len(data)
 
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@app.get('/method')
+@app.get('/patient')
 def method_get():
-    return {"method":"GET"}
+    return {"data":data}
 
-@app.post('/method')
-def method_post():
-
-    return {"method":"POST"}
-
-@app.put('/method')
-def method_put():
-    return {"method":"PUT"}
-
-@app.delete('/method')
-def method_delete():
-    return {"method":"DELETE"}
+@app.post('/patient')
+async def method_post(patient: Patient):
+    data.append({'name': patient.name, 'surename': patient.surename})
+    USER_NUM = len(data)
+    return {"id":USER_NUM-1, "patient": {"name":patient.name, "surename":patient.surename}}
