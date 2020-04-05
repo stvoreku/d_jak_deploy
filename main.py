5b914f7caf6f70a871f7c0385ff92e66bcfd594a
Jacek <stvoreku@gmail.com>
2020-04-05 17:17:33 +0200

    first commit

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ def hello_world():
 
 
 @app.get('/method')
-def method_get()
+def method_get():
     return {"method":"GET"}
 
 @app.post('/method')
-def method_post()
+def method_post():
     return {"method":"POST"}
 
 @app.put('/method')
-def method_put()
+def method_put():
     return {"method":"PUT"}
 
 @app.delete('/method')
-def method_delete()
+def method_delete():
     return {"method":"DELETE"}
