7e1f6383c994a05058e3144d48ae3cf14c3e824d
Jacek <stvoreku@gmail.com>
2020-04-26 20:47:03 +0200

    update get all

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ from pydantic import BaseModel
 
 USER_HASH = "Basic dHJ1ZG5ZOlBhQzEzTnQ=" #TB Stored in DB
 SESSION_TOKEN = ''
-class Patient(BaseModel):
-    name: str
-    surename: str
 
 app = FastAPI()
 app.secret_key = 'dlugi tajny klucz wszedl na plot i mruga'
+
 with open('json_data', 'w') as file:
     try:
         data = json.load(file.read)
@@ -24,32 +22,11 @@ USER_NUM = len(data)
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-
-@app.get('/patient')
-def get_all():
-    return(data)
-
-
-@app.get('/patient/{pk}')
-async def method_get(pk: int):
-    try:
-        patient = data[pk]
-    except IndexError:
-        raise HTTPException(status_code=204, detail="Content not found")
-    return patient
-
-@app.post('/patient')
-async def method_post(patient: Patient):
-    temp_num = len(data)
-    data.append({'name': patient.name, 'surename': patient.surename})
-    USER_NUM = len(data)
-    return {"id":temp_num, "patient": {"name":patient.name, "surename":patient.surename}}
-
 @app.get('/welcome')
 def welcome(SESSION_TOKEN: str = Cookie(None)):
     return {'msg':SESSION_TOKEN}
 
-@app.post('/login/')
+@app.post('/login')
 def login(user: str, password: str, response: Response):
     if decode(USER_HASH) == encode(user, password):
         SESSION_TOKEN = sha256(bytes(f"{USER_HASH}{app.secret_key}")).hexdigest()
---
+++ unused.py
@@ -0,0 +1,21 @@
+
+
+@app.get('/patient')
+def get_all():
+    return(data)
+
+
+@app.get('/patient/{pk}')
+async def method_get(pk: int):
+    try:
+        patient = data[pk]
+    except IndexError:
+        raise HTTPException(status_code=204, detail="Content not found")
+    return patient
+
+@app.post('/patient')
+async def method_post(patient: Patient):
+    temp_num = len(data)
+    data.append({'name': patient.name, 'surename': patient.surename})
+    USER_NUM = len(data)
+    return {"id":temp_num, "patient": {"name":patient.name, "surename":patient.surename}}
