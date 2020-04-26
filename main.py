d11a2697050c8ff3bba208194867ef0f8a93a99a
Jacek <stvoreku@gmail.com>
2020-04-26 22:00:02 +0200

    update get all

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Response, Cookie, HTTPException,Depends
+from fastapi import FastAPI, Response, Cookie, HTTPException,Depends, status
 from starlette.responses import RedirectResponse
 from hashlib import sha256
 from basicauth import encode, decode
 from pydantic import BaseModel
+import secrets
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 USER_HASH = "Basic dHJ1ZG5ZOlBhQzEzTnQ=" #TB Stored in DB
 SESSION_TOKEN = ''
@@ -10,7 +11,8 @@ SESSION_TOKEN = ''
 app = FastAPI()
 app.secret_key = 'dlugi tajny klucz wszedl na plot i mruga'
 security = HTTPBasic()
-app.users = {"trudnY": "PaC13Nt",
+
+
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
@@ -18,6 +20,19 @@ def hello_world():
 @app.get('/welcome')
 def welcome(SESSION_TOKEN: str = Cookie(None)):
     raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudny")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 
 @app.post('/login')
 def login(credentials: HTTPBasicCredentials = Depends(security)):
