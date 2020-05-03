a5942e6b0ff05faf0ebe3864d01bc39ebb9de256
Jacek <stvoreku@gmail.com>
2020-05-03 15:06:06 +0200

    update get all

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Response, Cookie, HTTPException,Depends, status, Request
+import sqlite3
 from starlette.responses import RedirectResponse
 from hashlib import sha256
 from pydantic import BaseModel
@@ -6,7 +7,7 @@ import json
 import secrets
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.templating import Jinja2Templates
-import sqlite3
+
 
 
 templates = Jinja2Templates(directory="templates")
@@ -45,11 +46,17 @@ def get_composer_tracks(name):
     res = [i for sub in res_raw for i in sub]
     return res
 
+def add_album(artist_id, name):
+    conn = sqlite3.connect('chinook.db')
+    c = conn.cursor()
+    cmd = "INSERT INTO albums (ArtistId, Title) VALUES (?, ?)"
+    c.execute(cmd, (artist_id, name,))
 
-class Patient(BaseModel):
-    name: str
-    surname: str
-
+    conn.row_factory = dict_factory
+    cmd = "SELECT * FROM albums WHERE Title = ? AND ArtistId = ?"
+    c.execute(cmd, (name, artist_id,))
+    res = c.fetchall()
+    return res
 
 @app.get('/tracks/composers')
 def comps(composer_name: str = None):
@@ -67,52 +74,22 @@ def tracks(page: int = 0, per_page: int = 10):
     return res[page*per_page:page*per_page+per_page]
 
 
+class Album(BaseModel):
+    title: str
+    artist_id: str
+
+
+@app.post('/albums')
+def post_albums(album: Album):
+    res = add_album(album.artist_id, album.title)
+    return res
+
 
 
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get('/welcome')
-def welcome(request: Request, session_token: str = Cookie(None)):
-    print('welcome, checking session', session_token, 'spodziewany:', app.sessions)
-    if session_token in app.sessions.keys():
-        return templates.TemplateResponse("welcomen.html", {"request": request, "user": app.sessions[session_token]})
-    raise HTTPException(status_code=401, detail="Unauthorized")
 
 
-def get_login(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    session_token = sha256(bytes(f"{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
-    print('token created: ', session_token)
-    app.sessions[session_token] = credentials.username
-    return session_token
-
-def check_session(token):
-    if token in app.sessions.keys():
-        return app.sessions[token]
-    else:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-
-@app.post('/login')
-def login(session_token: str = Depends(get_login)):
-    response = RedirectResponse(url='/welcome')
-    response.status_code = status.HTTP_302_FOUND
-    response.set_cookie(key="session_token", value=session_token)
-    return response
-
-@app.post('/logout')
-def logout(session_token: str = Cookie(None)):
-    check_session(session_token)
-    del app.sessions[session_token]
-    response = RedirectResponse(url='/')
-    response.status_code = status.HTTP_302_FOUND
-    response.set_cookie(key="session_token", value='')
-    return response
+print(add_album(1, "hells bells"))
--- unused.py
+++ unused.py
@@ -7,6 +7,13 @@ with open('json_data', 'w') as file:
 USER_NUM = len(data)
 
 
+
+
+class Patient(BaseModel):
+    name: str
+    surname: str
+
+
 @app.get('/patient')
 def get_all(session_token: str = Cookie(None)):
     print("asking for all patients,")
@@ -48,4 +55,50 @@ def delete_patient(pk: int, session_token: str = Cookie(None)):
         raise HTTPException(status_code=204, detail="Deleted earlier")
     response = Response()
     response.status_code=status.HTTP_204_NO_CONTENT
+    return response
+
+
+
+@app.get('/welcome')
+def welcome(request: Request, session_token: str = Cookie(None)):
+    print('welcome, checking session', session_token, 'spodziewany:', app.sessions)
+    if session_token in app.sessions.keys():
+        return templates.TemplateResponse("welcomen.html", {"request": request, "user": app.sessions[session_token]})
+    raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+def get_login(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "trudnY")
+    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    session_token = sha256(bytes(f"{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
+    print('token created: ', session_token)
+    app.sessions[session_token] = credentials.username
+    return session_token
+
+def check_session(token):
+    if token in app.sessions.keys():
+        return app.sessions[token]
+    else:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+@app.post('/login')
+def login(session_token: str = Depends(get_login)):
+    response = RedirectResponse(url='/welcome')
+    response.status_code = status.HTTP_302_FOUND
+    response.set_cookie(key="session_token", value=session_token)
+    return response
+
+@app.post('/logout')
+def logout(session_token: str = Cookie(None)):
+    check_session(session_token)
+    del app.sessions[session_token]
+    response = RedirectResponse(url='/')
+    response.status_code = status.HTTP_302_FOUND
+    response.set_cookie(key="session_token", value='')
     return response
